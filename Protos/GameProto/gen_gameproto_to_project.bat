ca615654e6126a4fe62e720b94ba1c5d69921d54
lvshiling <lvshiling@qq.com>
2025-05-13 16:15:11 +0800

    test c2s-slim

--- Protos/GameProto/gen_gameproto_to_project.bat
+++ Protos/GameProto/gen_gameproto_to_project.bat
@@ -19,6 +19,8 @@ set CODE_OUTPATH=%WORKSPACE%\UnityProject\Assets\GameScripts\HotFix\GameProto\Me
 echo "CODE_OUTPATH"
 
 %LUA_BIN% %SPROTODUMP_DLL% -cs %PROTOS_PATH%\Member.sproto -o %CODE_OUTPATH%\Member.cs
+%LUA_BIN% %SPROTODUMP_DLL% -cs %PROTOS_PATH%\c2s-slim.sproto -o %CODE_OUTPATH%\cs2-slim.cs
+rem %LUA_BIN% %SPROTODUMP_DLL% -cs %PROTOS_PATH%\s2c-slim.sproto -o %CODE_OUTPATH%\s2c-slim.cs
 
 cd %NOW_PATH%
 pause
